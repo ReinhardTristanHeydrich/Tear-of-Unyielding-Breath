6136b0a6067da17fd966c1c537a78bf26b8532ec
ReinhardTristanHeydrich <ReinhardTristanHeydrich@protonmail.com>
2025-08-13 12:51:42 -0300

    Silly test

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,43 @@ pub unsafe extern "C" fn DllMain(hmodule: usize, reason: u32) -> bool {
 
                 main_player.player_game_data.current_stamina = main_player.player_game_data.current_max_stamina;
             },
+            CSTaskGroupIndex::FrameBegin,
+        );
+        //==============================================================
+                task_system.run_recurring(
+            |_task_data: &FD4TaskData| {
+                let Some(main_player) = get_instance::<WorldChrMan>()
+                    .expect("No reflection data for WorldChrMan")
+                    .and_then(|wcm| wcm.main_player.as_mut())
+                else {return;};
+
+                main_player.player_game_data.current_stamina = main_player.player_game_data.current_max_stamina;
+            },
+            CSTaskGroupIndex::HavokBehavior,
+        );
+        //==============================================================
+                task_system.run_recurring(
+            |_task_data: &FD4TaskData| {
+                let Some(main_player) = get_instance::<WorldChrMan>()
+                    .expect("No reflection data for WorldChrMan")
+                    .and_then(|wcm| wcm.main_player.as_mut())
+                else {return;};
+
+                main_player.player_game_data.current_stamina = main_player.player_game_data.current_max_stamina;
+            },
             CSTaskGroupIndex::ChrIns_PostPhysics,
+        );
+        //==============================================================
+                task_system.run_recurring(
+            |_task_data: &FD4TaskData| {
+                let Some(main_player) = get_instance::<WorldChrMan>()
+                    .expect("No reflection data for WorldChrMan")
+                    .and_then(|wcm| wcm.main_player.as_mut())
+                else {return;};
+
+                main_player.player_game_data.current_stamina = main_player.player_game_data.current_max_stamina;
+            },
+            CSTaskGroupIndex::FrameEnd,
         );
         //==============================================================
     });
